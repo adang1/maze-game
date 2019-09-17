2e559760781b8199b753aa3d9de4c5ac4b24e891
Anh Dang <adang1@trinity.edu>
2019-09-16 19:38:56 -0500

    started

--- src/main/scala/graphicgame/Main.scala
+++ src/main/scala/graphicgame/Main.scala
@@ -9,8 +9,12 @@ import scalafx.scene.image.Image
  * This is a stub for the graphical game.
  */
 object Main extends JFXApp {
+	val canvas = new Canvas(800, 800)
+	val gc = canvas.graphics.Context2D
+	val renderer = new Renderer2D(gc, canvas)
+	val player = new Player
 	stage = new JFXApp.PrimaryStage {
-		title = "My Game" // Change this to match the theme of your game.
+		title = "2D Snipes" // Change this to match the theme of your game.
 		scene = new Scene(1000, 800) {
 		  // Put your code here.
 		}
