af75b206b4a86b4d1f062f5d6b992cc5938195ab
Anh Dang <adang1@trinity.edu>
2019-09-17 18:33:55 -0500

    drew a blank window

--- src/main/scala/graphicgame/Main.scala
+++ src/main/scala/graphicgame/Main.scala
@@ -18,17 +18,35 @@ import scalafx.scene.input.KeyCode
  * This is a stub for the graphical game.
  */
 object Main extends JFXApp {
-	val canvas = new Canvas(800, 800)
-	val gc = canvas.graphicsContext2D
-	val renderer = new Renderer2D(gc, 50)
-	val player = new Player(0, 0, ???)
-	val enemy = new Enemy(0, 0)
-	val entities = Seq(player, enemy)
-	val maze = RandomMaze
+
 	stage = new JFXApp.PrimaryStage {
 		title = "2D Snipes" // Change this to match the theme of your game.
 		scene = new Scene(1000, 800) {
-		  // Put your code here.
+			onKeyPressed = (ke: KeyEvent) => {
+				println("Key pressed")
+				ke.code match {
+					case KeyCode.Left =>
+					case _ =>
+				}
+			}
+	// 			val canvas = new Canvas(800, 800)
+	// val gc = canvas.graphicsContext2D
+	// val renderer = new Renderer2D(gc, 50)
+	// val player = new Player(0, 0, ???)
+	// val enemy = new Enemy(0, 0)
+	// val entities = Seq(player, enemy)
+	// val maze = RandomMaze
+			var lastTime = -1L
+			val timer = AnimationTimer(time => {
+				if (lastTime > 0) {
+				val delay = (time - lastTime)/1e9
+				println(delay)
+				// level.update()
+				// renderer2D.render(level)
+			}
+			lastTime = time
+		})
+			timer.start()
 		}
 	}
 }
